38560c6d3c9b81c0811e9b1e9c1a877517fdae44
wafik20 <wafik.wt@gmail.com>
2023-03-25 09:19:37 -0400

    routes update

--- API/routes/video.js
+++ API/routes/video.js
@@ -5,8 +5,21 @@ router.get("/test", (req, res)=>{
     res.send("test video!")
 })
 
-router.post("/", (req,res)=>{
-    
+router.post("/", async (req,res)=>{
+    //url = req.params.url
+    console.log({
+        req
+    });
+    const newVideo = new Video({
+        questions: req.body.questions,
+        url: req.body.url
+      });
+      try {
+        const savedVideo = await newVideo.save();
+        res.status(201).json(savedVideo);
+      } catch (err) {
+        res.status(500).json(err);
+      }
 })
 
 module.exports = router
